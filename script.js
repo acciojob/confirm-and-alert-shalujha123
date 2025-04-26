86c01a89425262f198d8e03bb2b224743d2af5bf
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-26 22:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let confirmRes = confirm("Do you want to proceed?")
+
+if(confirmRes){
+	alert("You clicked OK. Proceeding...")
+}else{
+	alert("You clicked Cancel. Exiting...")
+}
